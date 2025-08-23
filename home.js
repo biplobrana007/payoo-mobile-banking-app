a0eab6e31b145cac3754535794fd045001828ddf
biplobrana007 <mubiplobrana@gmail.com>
2025-08-23 20:54:44 +0600

    cashout section done

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
+/*log out*/
+document.getElementById("log-out-btn").addEventListener("click", (event) => {
+  event.preventDefault();
+  window.location.href = "./index.html";
+});
+
+/*add money*/
+
 document.getElementById("add-money-btn").addEventListener("click", (event) => {
   event.preventDefault();
   const userBank = document.getElementById("bank").value;
@@ -30,7 +38,46 @@ document.getElementById("add-money-btn").addEventListener("click", (event) => {
   }
 });
 
-document.getElementById("log-out-btn").addEventListener("click", (event) => {
+document.getElementById("add-money").addEventListener("click", () => {
+  document.getElementById("add-money-div").style.display = "block";
+  document.getElementById("cash-out-div").style.display = "none";
+});
+
+/*cash out*/
+
+document
+  .getElementById("widrw-money-btn")
+  .addEventListener("click", (event) => {
     event.preventDefault();
-    window.location.href = "./index.html"
+    const userNumber = document.getElementById("agent-number").value;
+    const userWidrwAmount = document.getElementById("widrw-amount").value;
+    const userPinNumber = document.getElementById("pin-to-widrw").value;
+
+    let yourAmount = document.getElementById("your-amount").innerText;
+    yourAmount = Number(yourAmount) - Number(userWidrwAmount);
+
+    const pinNumber = 1234;
+
+    if (userNumber === "") {
+      return alert("plese enter agent number");
+    }
+
+    if (userWidrwAmount === "") {
+      return alert("plese enter the amount you want to withdraw");
+    }
+
+    if (userPinNumber === "") {
+      return alert("plese enter the pin");
+    }
+
+    if (Number(userPinNumber) === pinNumber) {
+      return (document.getElementById("your-amount").innerText = yourAmount);
+    } else {
+      return alert("enter valid pin");
+    }
+  });
+
+document.getElementById("cash-out").addEventListener("click", () => {
+  document.getElementById("cash-out-div").style.display = "block";
+  document.getElementById("add-money-div").style.display = "none";
 });
