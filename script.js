e3ac47d161f39da8e50bb75d8060c041c77b8c69
biplobrana007 <mubiplobrana@gmail.com>
2025-08-23 14:12:52 +0600

    made the login btn dynamic with js with click event

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("login-btn").addEventListener("click", (event) => {
+  event.preventDefault();
+  const phoneNumber = 123456789;
+  const pinNumber = 1234;
+  const userPhoneNumber = parseInt(
+    document.getElementById("mobile-number").value
+  );
+  const userPinNumber = parseInt(document.getElementById("pin-number").value);
+  if (userPhoneNumber === phoneNumber && userPinNumber === pinNumber) {
+    window.location.href = "./home.html";
+  } else {
+    console.log("invalid");
+  }
+});
